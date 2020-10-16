d8c267cf26b765afd98847552b8746fd2498a7f5
Treeboy2762 <44747571+Treeboy2762@users.noreply.github.com>
2020-10-17 00:15:24 +0900

    Add files via upload

---
+++ Keypad.ino
@@ -0,0 +1,29 @@
+#include <Keypad.h>
+
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+byte rowPins[ROWS] = {9,8,7,6};
+byte colPins[COLS] = {5,4,3,2};
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  char key = keypad.getKey();
+  if (key) {
+    Serial.println(key);
+  }
+}
